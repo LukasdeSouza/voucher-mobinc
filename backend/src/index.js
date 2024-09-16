e5cf9e9a278deae9f231aabbc3a8e96ac484f0db
unknown <lucasdesouzasilva112@gmail.com>
2024-09-16 16:55:50 -0300

    ok

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,9 @@
-const express = require('express');
-const cors = require('cors');
-const connectDB = require('./db')
-const voucherRoutes = require('./routes/voucherRoutes');
-const authRoutes = require('./routes/authRoutes');
-require('dotenv').config();
+const express = require("express");
+const cors = require("cors");
+const connectDB = require("./db");
+const voucherRoutes = require("./routes/voucherRoutes");
+const authRoutes = require("./routes/authRoutes");
+require("dotenv").config();
 
 const app = express();
 app.use(cors());
@@ -15,8 +15,12 @@ connectDB();
 app.use(express.json());
 
 // Rotas
-app.use('/api/vouchers', voucherRoutes);
-app.use('/api/auth', authRoutes);
+app.use("/api/vouchers", voucherRoutes);
+app.use("/api/auth", authRoutes);
+
+app.user("/", (req, res) => {
+  return res.status(200).json({ message: "API is running" });
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
