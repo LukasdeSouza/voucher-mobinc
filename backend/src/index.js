61fd8126387d46f695643ff6a7b6d1b063cd18c9
unknown <lucasdesouzasilva112@gmail.com>
2024-09-16 18:47:34 -0300

    corrigindo caquinha

--- backend/src/index.js
+++ backend/src/index.js
@@ -18,9 +18,6 @@ app.use(express.json());
 app.use("/api/vouchers", voucherRoutes);
 app.use("/api/auth", authRoutes);
 
-app.use("/", (req, res) => {
-  return res.status(200).json({ message: "API is running" });
-});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
