ef2e30e15b296197f83937d733379d7d3e10f4da
unknown <lucasdesouzasilva112@gmail.com>
2024-10-14 18:00:35 -0300

    correção das rotas

--- backend/index.js
+++ backend/index.js
@@ -12,12 +12,12 @@ connectDB();
 
 app.use(express.json());
 
+app.use("/api/vouchers", voucherRoutes);
+app.use("/api/auth", authRoutes);
+
 app.use("/", (req, res) => {
   res.send("API is running...");
 })
-
-app.use("/api/vouchers", voucherRoutes);
-app.use("/api/auth", authRoutes);
 
 
 const PORT = process.env.PORT || 5000;
