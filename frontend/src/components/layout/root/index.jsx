1ad255e6430bfe373447ca50fbd6a8f120ddac62
unknown <lucasdesouzasilva112@gmail.com>
2024-10-16 15:27:00 -0300

    adicionando navegação no rootlayout

--- frontend/src/components/layout/root/index.jsx
+++ frontend/src/components/layout/root/index.jsx
@@ -1,6 +1,6 @@
-import { Outlet } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 import HeaderNavigation from "../../header";
-import {Toaster} from "react-hot-toast";
+import { Toaster } from "react-hot-toast";
 import SplashPage from "../../../pages/splash";
 import { useState } from "react";
 
@@ -11,18 +11,23 @@ const RootLayout = () => {
     setLoading(false);
   }, 2500);
 
-  if(loading) {
-    return <SplashPage/>
+  if (loading) {
+    return <SplashPage />;
   } else {
     return (
       <div className="w-screen h-full bg-[#0000]">
+        <Link
+          className="absolute right-4 top-4 text-white text-sm z-20 hover:underline"
+          to="/resgatar"
+        >
+          Resgatar Voucher
+        </Link>
         {/* <HeaderNavigation /> */}
         <Outlet />
         <Toaster />
       </div>
     );
   }
-
 };
 
 export default RootLayout;
