1c8b17782b290faa283cde88b6626ed4c364d613
LukasdeSouza <lucasdesouzasilva112@gmail.com>
2026-03-06 14:09:39 -0300

    update

--- backend/src/index.js
+++ backend/src/index.js
@@ -302,4 +302,4 @@ app.get("/getAll", async (req, res) => {
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
-// module.exports = app;
+module.exports = app;
